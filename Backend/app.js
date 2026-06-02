e45b406903d1caf79613e22377477caa52fa9982
SkyWik <mas.dimas5758@gmail.com>
2026-06-03 02:42:42 +0700

    Fix Error Deploying : Tes tambah route kosong

--- Backend/app.js
+++ Backend/app.js
@@ -14,6 +14,13 @@ app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '2mb', extended: true }));
 
+app.get('/', (req, res) => {
+  res.json({
+    status: "success",
+    message: "Server Backend Capstone Financial API Berhasil Berjalan di Vercel!"
+  });
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/profile', profileRoutes);
